db69019cab5ccb8d29f8e53e9e1353790defb08f
antiSymmetric <pixeldrift.adv@proton.me>
2025-08-28 11:21:39 +0530

    Update use-get-workspaces.ts

--- src/features/workspaces/api/use-get-workspaces.ts
+++ src/features/workspaces/api/use-get-workspaces.ts
@@ -1,18 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-
 import { client } from '@/lib/hono';
+import type { Workspace } from '@/features/workspaces/types';
 
 export const useGetWorkspaces = () => {
-  const query = useQuery({
+  const query = useQuery<Workspace[]>({
     queryKey: ['workspaces'],
-    queryFn: async () => {
+    queryFn: async (): Promise<Workspace[]> => {
       const response = await client.api.workspaces.$get();
 
       if (!response.ok) throw new Error('Failed to fetch workspaces.');
 
       const { data } = await response.json();
 
-      return data;
+      return data as Workspace[];
     },
   });
 
