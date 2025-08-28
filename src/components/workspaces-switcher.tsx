e4a3d6954947523cd998eaabd2f6b556337613b6
antiSymmetric <pixeldrift.adv@proton.me>
2025-08-28 11:25:15 +0530

    Update workspaces-switcher.tsx

--- src/components/workspaces-switcher.tsx
+++ src/components/workspaces-switcher.tsx
@@ -35,11 +35,10 @@ export const WorkspaceSwitcher = () => {
         </SelectTrigger>
 
         <SelectContent>
-          {workspaces?.documents.map((workspace) => (
+          {workspaces?.map((workspace) => (
             <SelectItem key={workspace.$id} value={workspace.$id}>
               <div className="flex items-center justify-start gap-3 font-medium">
                 <WorkspaceAvatar name={workspace.name} image={workspace.imageUrl} />
-
                 <span className="truncate">{workspace.name}</span>
               </div>
             </SelectItem>
